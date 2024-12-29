e8ff84a789e6ca7696b28ba8dd6a56c5a89d5895
Cristiangpbf <cristiangpbf@gmail.com>
2024-12-29 12:13:41 -0500

    Añadir al informe reconocimiento facial.

--- reconocimientoFacial.py
+++ reconocimientoFacial.py
@@ -15,7 +15,7 @@ face_recognizer = cv2.face.LBPHFaceRecognizer_create()
 face_recognizer.read('modeloLBPHFace.xml')
 
 cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-# cap = cv2.VideoCapture('Video.mp4')
+# cap = cv2.VideoCapture('recursos_reconocimiento_facial/Videos/Ambar.mp4')
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -36,10 +36,12 @@ while True:
 
         # LBPHFace
         if result[1] < 70:
-            cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x, y - 25), 2, 1.1, (0, 255, 0), 1, cv2.LINE_AA)
+            cv2.putText(frame, '{}'.format(imagePaths[result[0]]), (x, y - 25), 2, 1.1,
+                        (0, 255, 0), 1, cv2.LINE_AA)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         else:
-            cv2.putText(frame, 'Desconocido', (x, y - 20), 2, 0.8, (0, 0, 255), 1, cv2.LINE_AA)
+            cv2.putText(frame, 'Desconocido', (x, y - 20), 2, 0.8, (0, 0, 255),
+                        1, cv2.LINE_AA)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
         '''
